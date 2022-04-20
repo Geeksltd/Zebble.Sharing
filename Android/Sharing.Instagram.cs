5e29fbf2f46ec82146111874662abfbe58ef4782
Ali Yousefi <ali.visual.studio@gmail.com>
2022-04-20 17:24:00 +0100

    add support for instagram

---
+++ Android/Sharing.Instagram.cs
@@ -0,0 +1,29 @@
+﻿
+using Android.Content;
+using Android.Net;
+using AndroidX.Core.Content;
+using Java.IO;
+
+namespace Zebble.Device
+{
+    public partial class Sharing
+    {
+        public partial class Instagram
+        {
+            public static void ShareFile(string url, string title)
+            {
+                var shareIntent = new Intent(Intent.ActionSend);
+                shareIntent.SetType("image/*");
+                shareIntent.PutExtra(Intent.ExtraTitle, title);
+
+                File myFiles = new File(url);
+                Uri doneUri = FileProvider.GetUriForFile(UIRuntime.CurrentActivity.ApplicationContext,
+                        UIRuntime.CurrentActivity.PackageName + ".fileprovider", myFiles);
+
+                shareIntent.PutExtra(Intent.ExtraStream, doneUri);
+                shareIntent.SetPackage("com.instagram.android");
+                UIRuntime.CurrentActivity.StartActivity(shareIntent);
+            }
+        }
+    }
+}
